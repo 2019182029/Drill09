5c986648be6707a3068489d9704df0ab3c987ac4
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-10-11 21:50:17 +0900

    IDLE 상태 구현

--- boy.py
+++ boy.py
@@ -1,6 +1,11 @@
 from pico2d import *
 
 
+# 1. AutoRun 클래스를 추가하여 'a'키를 눌렀을 때 키 조작 없이도 캐릭터가 움직이는 것을 구현한다.
+# 2. 자동 무적 런 상태에서 5초 이상 경과했을 때 IDLE 상태로 돌아가는 것을 구현한다.
+# 3. 캐릭터의 속도와 크기를 증가시키고, 화면의 좌우측 끝에서 자동으로 방향 전환하는 것을 구현한다.
+
+
 def a_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
 
@@ -21,11 +26,15 @@ class Idle:
 
     @staticmethod
     def do(boy):
-        pass
+        boy.frame = (boy.frame + 1) % 8
 
     @staticmethod
     def draw(boy):
-        pass
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+
+class AutoRun:
+    pass
 
 
 class StateMachine:
@@ -44,14 +53,14 @@ class StateMachine:
         self.cur_state.draw(self.boy)
 
     def handle_event(self, event):
-        for check_event, next_state in self.transition[self.cur_state].items():
-            if check_event(event):
-                self.cur_state.exit(self.boy, event)
-                self.cur_state = next_state
-                self.cur_state.enter(self.boy, event)
-                return True
-        return False
-
+        # for check_event, next_state in self.transition[self.cur_state].items():
+        #     if check_event(event):
+        #         self.cur_state.exit(self.boy, event)
+        #         self.cur_state = next_state
+        #         self.cur_state.enter(self.boy, event)
+        #         return True
+        # return False
+        pass
 
 class Boy:
     def __init__(self):
