7e201524b9a49eceac756e43b04f2511b4316257
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-10-11 22:40:43 +0900

    기존 boy.py와 병합, 소스 코드 완성

--- boy.py
+++ boy.py
@@ -1,29 +1,102 @@
 from pico2d import *
+import math
 
 
 def a_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
 
 
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+
 def time_out(e):
     return e[0] == 'TIME_OUT'
+
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 
 class Idle:
 
     @staticmethod
     def enter(boy, event):
-        print("Enter Idle")
-        boy.x, boy.y = 400, 90
-        boy.action = 3
+        if boy.action == 0:
+            boy.action = 2
+        elif boy.action == 1:
+            boy.action = 3
+        boy.dir = 0
+        boy.frame = 0
+        boy.idle_start_time = get_time()
 
     @staticmethod
-    def exit(boy):
-        print("Exit Idle")
+    def exit(boy, event):
+        pass
 
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
+        if (get_time() - boy.idle_start_time > 3):
+            boy.state_machine.handle_event(('TIME_OUT', 0))
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+
+class Sleep:
+
+    @staticmethod
+    def enter(boy, event):
+        boy.frame = 0
+
+    @staticmethod
+    def exit(boy, event):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame % 8) + 1
+
+    @staticmethod
+    def draw(boy):
+        if boy.action == 2:
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, math.pi / -2, '',
+                                          boy.x + 25, boy.y - 25, 100, 100)
+        else:
+            boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, math.pi / 2, '',
+                                          boy.x - 25, boy.y - 25, 100, 100)
+
+
+class Run:
+    @staticmethod
+    def enter(boy, e):
+        if (right_down(e) or left_up(e)):
+            boy.dir, boy.action = 1, 1
+        elif (left_down(e) or right_up(e)):
+            boy.dir, boy.action = -1, 0
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.dir * 5
 
     @staticmethod
     def draw(boy):
@@ -35,12 +108,16 @@ class AutoRun:
     def enter(boy, event):
         print("Enter AutoRun")
         boy.a_up_time = get_time()
-        boy.action = 0
-        boy.dir = -10
+        if (boy.action == 3):
+            boy.action = 1
+            boy.dir = 10
+        else:
+            boy.action = 0
+            boy.dir = -10
 
     @staticmethod
-    def exit(boy):
-        print("Exit AutoRun")
+    def exit(boy, event):
+        pass
 
     @staticmethod
     def do(boy):
@@ -67,7 +144,9 @@ class StateMachine:
         self.boy = boy
         self.cur_state = Idle
         self.transition = {
-            Idle: {a_down: AutoRun},
+            Idle: {a_down: AutoRun, right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
+            Run: {right_down: Idle, left_down: Idle, left_up: Idle, right_up: Idle},
+            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},
             AutoRun: {time_out: Idle}
         }
 
@@ -83,7 +162,7 @@ class StateMachine:
     def handle_event(self, event):
         for check_event, next_state in self.transition[self.cur_state].items():
             if check_event(event):
-                self.cur_state.exit(self.boy)
+                self.cur_state.exit(self.boy, event)
                 self.cur_state = next_state
                 self.cur_state.enter(self.boy, event)
                 return True
